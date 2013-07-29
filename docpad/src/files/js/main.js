4dd7dc1b7a398a6e14f1a8ec20ac6afb12e1131f
Mark <mark.crawley@uk.ibm.com>
2013-07-29 10:50:54 +0100

    Added Flickr set tag functionality

--- docpad/src/files/js/main.js
+++ docpad/src/files/js/main.js
@@ -1,35 +1,52 @@
-function loadPhotos(date) {
-	console.log(date);
+function handleFlickrPhotos(data) {
+	var photoType = "s"; // small square
+	var list = $("<ul></ul>");
+	var rootNode;
+	if (data.photoset) {
+		rootNode = data.photoset.photo;
+	} else {
+		rootNode = data.photos.photo;
+	}
+	$.each(rootNode, function (i, photo) {
+		var img = $("<img/>")
+		.attr("src", "http://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + photoType + ".jpg")
+		.attr("width", "75")
+		.attr("height", "75")
+		.attr("alt", photo.title);
+		var link = $("<a/>")
+		.attr("href", "http://www.flickr.com/photos/crawlem/" + photo.id);
+		var fullLink = link.append(img);
+		var li = $("<li/>").append(fullLink);
+		$(list).append(li);
+	});
+	if (rootNode.length > 0) {
+		$("#photos").append("<h2>Photos</h2>");
+		$("#photos").append(list);
+	}
+}
+
+function handleFlickrError(o, s) {
+	alert(s);
+}
+
+function loadPhotoDay(date) {
 	if ($("#photos")) {
 		var jqxhr = $.ajax({
 			url: "http://ycpi.api.flickr.com/services/rest/?method=flickr.photos.search&user_id=90014740@N00&min_taken_date=" + date + " 00:00:00&max_taken_date=" + date + " 23:59:59&sort=date-taken-asc&format=json&nojsoncallback=1&api_key=6b44b7eaf862acb6abf898f5bc3882fb",
 			dataType: "json"
 		})
-		.done(function(data) {
-			var photoType = "s"; // small square
-			// if (data.photos.photo.length <= 5) {
-			//   photoType = "q"; // larger square
-			// }
-			var list = $("<ul></ul>");
-			$.each(data.photos.photo, function (i, photo) {
-				var img = $("<img/>")
-				.attr("src", "http://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + photoType + ".jpg")
-				.attr("width", "75")
-				.attr("height", "75")
-				.attr("alt", photo.title);
-				var link = $("<a/>")
-				.attr("href", "http://www.flickr.com/photos/crawlem/" + photo.id);
-				var fullLink = link.append(img);
-				var li = $("<li/>").append(fullLink);
-				$(list).append(li);
-			});
-			if (data.photos.photo.length > 0) {
-				$("#photos").append("<h2>Photos</h2>");
-				$("#photos").append(list);
-			}
+		.done(handleFlickrPhotos)
+		.fail(handleFlickrError);
+	}
+}
+
+function loadPhotoSet(setId) {
+	if ($("#photos")) {
+		var jqxhr = $.ajax({
+			url: "http://ycpi.api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&photoset_id=" + setId + "&format=json&nojsoncallback=1&api_key=6b44b7eaf862acb6abf898f5bc3882fb",
+			dataType: "json"
 		})
-		.fail(function(o, s) {
-			alert(s);
-		});
+		.done(handleFlickrPhotos)
+		.fail(handleFlickrError);
 	}
 }
